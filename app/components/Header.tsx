1950af9a2ff7322b6fb21758e4fcabc57bb0b46d
Josselin <obarayego@gmail.com>
2025-06-02 19:02:15 +0200

    deploy

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -31,8 +31,8 @@ export function Header({
           <img
             src='https://cdn.shopify.com/s/files/1/0906/1384/2263/files/ejiogbe.svg?v=1748831331'
             alt={shop.name}
-            width={56}
-            height={56}
+            width={60}
+            height={60}
           />
         </h1>
       </NavLink>
